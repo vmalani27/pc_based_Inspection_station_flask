4481194678b15067bfaa8a2ced2840bab8deef5b
flash-275 <vanshmalani27@gmail.com>
2025-09-04 15:52:14 +0530

    localhost changed for deployment

--- main.py
+++ main.py
@@ -803,4 +803,4 @@ def delete_user_entries():
 # ----------------------------------------------------------------------------
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
